a8750d8b24280dfd2c9c3478eec8f2ae704550c7
Beauty kumari <kbeauty193@gmail.com>
2024-08-19 23:57:11 +0530

    'push'

--- src/ProjectStudentDataManagement/DataBaseConnection.java
+++ src/ProjectStudentDataManagement/DataBaseConnection.java
@@ -4,3 +4,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
+
+public class DataBaseConnection {
+    public static Connection dbConn() {
