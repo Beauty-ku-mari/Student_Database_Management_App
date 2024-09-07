22f1536b3cdc78b302061fe1f85a7fed6c9e0317
Beauty kumari <kbeauty193@gmail.com>
2024-09-07 10:59:55 +0530

    'commit'

--- src/ProjectStudentDataManagement/Login.java
+++ src/ProjectStudentDataManagement/Login.java
@@ -37,77 +37,77 @@ public class Login {
         });
     }
 
- public Login() {
+    public Login() {
         sdm = DataBaseConnection.dbConn();
         initialize();
     }
 
-private void initialize() {
+    private void initialize() {
         loginsdm = new JFrame();
         loginsdm.getContentPane().setBackground(new Color(0, 0, 0));
         loginsdm.setTitle("STUDENT DATA MANAGEMENT Login\r\n");
-loginsdm.setIconImage(Toolkit.getDefaultToolkit().getImage("resources/images/unnamed.png"));
+        loginsdm.setIconImage(Toolkit.getDefaultToolkit().getImage("resources/images/unnamed.png"));
         loginsdm.setBounds(100, 110, 858, 559);
- loginsdm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loginsdm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         loginsdm.getContentPane().setLayout(null);
 
-tf1 = new JTextField();
+        tf1 = new JTextField();
         tf1.setBackground(new Color(175, 238, 238));
         tf1.setBounds(117, 173, 265, 41);
-loginsdm.getContentPane().add(tf1);
+        loginsdm.getContentPane().add(tf1);
         tf1.setColumns(10);
 
-JLabel lblNewLabel = new JLabel("Enter Username");
+        JLabel lblNewLabel = new JLabel("Enter Username");
         lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 20));
- lblNewLabel.setForeground(new Color(135, 206, 250));
+        lblNewLabel.setForeground(new Color(135, 206, 250));
         lblNewLabel.setBounds(118, 131, 233, 41);
         loginsdm.getContentPane().add(lblNewLabel);
 
-JLabel lblEnterPassword = new JLabel("Enter Password");
+        JLabel lblEnterPassword = new JLabel("Enter Password");
         lblEnterPassword.setFont(new Font("Tahoma", Font.BOLD, 20));
         lblEnterPassword.setForeground(new Color(135, 206, 250));
-lblEnterPassword.setBounds(117, 259, 198, 33);
+        lblEnterPassword.setBounds(117, 259, 198, 33);
         loginsdm.getContentPane().add(lblEnterPassword);
 
- tf2 = new JPasswordField();
+        tf2 = new JPasswordField();
         tf2.setBackground(new Color(175, 238, 238));
         tf2.setBounds(117, 292, 265, 41);
-loginsdm.getContentPane().add(tf2);
+        loginsdm.getContentPane().add(tf2);
 
-JButton b = new JButton("Login");
+        JButton b = new JButton("Login");
         b.setFont(new Font("Tahoma", Font.BOLD, 20));
-b.addActionListener(new ActionListener() {
+        b.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-String q = "SELECT * from login where username = ? and pass = ?";
+                String q = "SELECT * from login where username = ? and pass = ?";
                 try {
-PreparedStatement ps = sdm.prepareStatement(q);
+                    PreparedStatement ps = sdm.prepareStatement(q);
                     ps.setString(1, tf1.getText());
-ps.setString(2, String.valueOf(tf2.getPassword()));
+                    ps.setString(2, String.valueOf(tf2.getPassword()));
                     ResultSet rs = ps.executeQuery();
                     if (rs.next()) {
- JOptionPane.showMessageDialog(null, "Welcome to Student Data Management");
+                        JOptionPane.showMessageDialog(null, "Welcome to Student Data Management");
                         new StudentDataManagement().setVisible(true);
                     } else {
- JOptionPane.showMessageDialog(null, "Username / Password Invalid!");
+                        JOptionPane.showMessageDialog(null, "Username / Password Invalid!");
                     }
                 } catch (SQLException e1) {
                     e1.printStackTrace();
                 }
             }
         });
- b.setForeground(new Color(0, 0, 0));
+        b.setForeground(new Color(0, 0, 0));
         b.setBackground(new Color(30, 144, 255));
-b.setBounds(158, 361, 157, 41);
+        b.setBounds(158, 361, 157, 41);
         loginsdm.getContentPane().add(b);
 
-JLabel lblNewLabel_1 = new JLabel("");
+        JLabel lblNewLabel_1 = new JLabel("");
         lblNewLabel_1.setIcon(new ImageIcon("resources/images/b5.jpg"));
         lblNewLabel_1.setBounds(497, 0, 357, 416);
- loginsdm.getContentPane().add(lblNewLabel_1);
+        loginsdm.getContentPane().add(lblNewLabel_1);
 
         JLabel lblNewLabel_2 = new JLabel("STUDENT DATA MANAGEMENT");
         lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 24));
-lblNewLabel_2.setForeground(new Color(0, 255, 255));
+        lblNewLabel_2.setForeground(new Color(0, 255, 255));
         lblNewLabel_2.setBounds(33, 38, 414, 50);
         loginsdm.getContentPane().add(lblNewLabel_2);
     }
---
+++ src/ProjectStudentDataManagement/StudentDataManagement.java
@@ -0,0 +1,16 @@
+package ProjectStudentDataManagement;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
